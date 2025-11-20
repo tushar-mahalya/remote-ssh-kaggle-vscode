23efcf58a2a2397ca685968256413e3668adec72
Tushar Sharma <tusharmahalya@gmail.com>
2025-11-21 03:52:13 +0530

    Add installation of nvidia-utils-515

--- install_cuda.sh
+++ install_cuda.sh
@@ -8,6 +8,7 @@ sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt install nvidia-utils-515 -y
 sudo apt-get update
 sudo apt-get -y install cuda
 sudo rm /bin/nvidia-smi
